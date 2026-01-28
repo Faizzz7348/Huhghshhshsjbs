2f4e7c2e0881197bf76f20a45f19d33a4a26eb70
Journey Shopper <86806480+Faizzz7348@users.noreply.github.com>
2026-01-28 08:23:50 +0000

    Add networkTimeoutSeconds to next-pwa type definition

--- next-pwa.d.ts
+++ next-pwa.d.ts
@@ -18,6 +18,7 @@ declare module 'next-pwa' {
         cacheableResponse?: {
           statuses?: number[];
         };
+        networkTimeoutSeconds?: number;
       };
     }>;
     publicExcludes?: string[];
